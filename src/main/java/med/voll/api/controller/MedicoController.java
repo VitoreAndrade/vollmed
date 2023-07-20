cec945edf6ab92b83f8bdd6cfedfb07b80504cc6
VITOR ANDRADE SOUSA <730549653@adm.unifor.br@DTEC65045565.unifor.br>
2023-07-20 16:46:32 -0400

    Primeiro commit

---
+++ src/main/java/med/voll/api/controller/MedicoController.java
@@ -0,0 +1,33 @@
+package med.voll.api.controller;
+
+import jakarta.transaction.Transactional;
+import jakarta.validation.Valid;
+import med.voll.api.endereco.Endereco;
+import med.voll.api.medico.DadosListagemMedico;
+import med.voll.api.medico.Medico;
+import med.voll.api.medico.MedicoRepository;
+import med.voll.api.medico.dadosCadastroMedicos;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/medicos")
+public class MedicoController {
+    @Autowired
+    private MedicoRepository repository;
+    @PostMapping
+    @Transactional
+    public void cadastrar(@RequestBody @Valid dadosCadastroMedicos dados){
+        repository.save(new Medico(dados));
+    }
+@GetMapping
+    public Page<DadosListagemMedico> listarMedico(@PageableDefault(size = 10, sort = {"nome"}) Pageable paginacao){
+        return repository.findAll(paginacao).map(DadosListagemMedico::new);
+
+    }
+}
