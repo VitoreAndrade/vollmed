b025713db2148826463dab62214e4a7c17a24a9d
Vitao <vitor.andradee158@gmail.com>
2023-07-26 17:03:05 -0400

    atualizacao

--- src/main/java/med/voll/api/controller/ControllerPaciente.java
+++ src/main/java/med/voll/api/controller/ControllerPaciente.java
@@ -2,6 +2,8 @@ package med.voll.api.paciente;
 
 import jakarta.transaction.Transactional;
 import jakarta.validation.Valid;
+import med.voll.api.medico.Medico;
+import med.voll.api.medico.dadosCadastroMedicos;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -10,9 +12,15 @@ import org.springframework.web.bind.annotation.*;
 public class ControllerPaciente {
     @Autowired
     private PacienteRepository repository;
+
+
+    @PostMapping
+    @Transactional
+    public void cadastrar(@RequestBody @Valid dadosCadastrosPacientes dados){
+        repository.save(new Paciente(dados));
+    }
     @PutMapping
     @Transactional
-
     public void atualizar(@RequestBody @Valid dadosAtualizacaoPacientes dadosAtualizadosPacientes){
         var paciente = repository.getReferenceById(dadosAtualizadosPacientes.id());
         paciente.atualizarInformacoesPaciente(dadosAtualizadosPacientes);
